6d52a254472e8d102f3d6a136650368fd7147bd8
Hugo Schmitt <hugows@gmail.com>
2015-06-20 18:34:05 -0300

    Split to file

---
+++ result.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/nsf/termbox-go"
+)
+
+type Result struct {
+	contents        string       // user input is matched against lowercase version
+	displayContents string       // original filename (or line) to display
+	highlighted     map[int]bool // hashmap of the characters to be highlighted
+	marked          bool         // true when the current line is selected
+	score           int          // what is the score for this particular result?
+}
+
+const CHECKMARK_CHAR = " ✓"
+
+func (res Result) Draw(x, y, w int, selected bool) {
+	CHECKMARK_PAD := strings.Repeat(" ", len(CHECKMARK_CHAR))
+	const coldef = termbox.ColorDefault
+
+	color := coldef
+
+	if selected {
+		color = color | termbox.AttrReverse
+	}
+
+	line := ""
+	line += fmt.Sprintf("%4d ", res.score)
+	if res.marked {
+		line += CHECKMARK_CHAR
+	} else {
+		line += CHECKMARK_PAD
+	}
+	tclearcolor(x, y, w, 1, color)
+	tbprint(x, y, color, color, line)
+	x += len(line)
+
+	for idx, c := range res.displayContents {
+		fg := color
+		bg := color
+		if res.highlighted[idx] {
+			fg = termbox.ColorGreen | termbox.AttrBold
+		}
+		termbox.SetCell(x, y, c, fg, bg)
+		x++
+	}
+
+	// tclear(x, y, w, 1)
+}
--- results.go
+++ results.go
@@ -1,56 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"sort"
 	"strings"
 
 	"github.com/nsf/termbox-go"
 )
-
-type Result struct {
-	contents        string       // user input is matched against lowercase version
-	displayContents string       // original filename (or line) to display
-	highlighted     map[int]bool // hashmap of the characters to be highlighted
-	marked          bool         // true when the current line is selected
-	score           int          // what is the score for this particular result?
-}
-
-const CHECKMARK_CHAR = " ✓"
-
-func (res Result) Draw(x, y, w int, selected bool) {
-	CHECKMARK_PAD := strings.Repeat(" ", len(CHECKMARK_CHAR))
-	const coldef = termbox.ColorDefault
-
-	color := coldef
-
-	if selected {
-		color = color | termbox.AttrReverse
-	}
-
-	line := ""
-	line += fmt.Sprintf("%4d ", res.score)
-	if res.marked {
-		line += CHECKMARK_CHAR
-	} else {
-		line += CHECKMARK_PAD
-	}
-	tclearcolor(x, y, w, 1, color)
-	tbprint(x, y, color, color, line)
-	x += len(line)
-
-	for idx, c := range res.displayContents {
-		fg := color
-		bg := color
-		if res.highlighted[idx] {
-			fg = termbox.ColorGreen | termbox.AttrBold
-		}
-		termbox.SetCell(x, y, c, fg, bg)
-		x++
-	}
-
-	// tclear(x, y, w, 1)
-}
 
 type ResultCollection []*Result
 
@@ -245,6 +200,9 @@ Loop:
 
 	// TODO: better cursor behaviouree
 	r.SelectFirst()
+
+	close(quit)
+	close(keypressed)
 }
 
 func (r *Results) GetSelected() *Result {
