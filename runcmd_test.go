75f0a2ec24a6cc210f6d3365f0576aa0a216c845
Hugo Schmitt <hugows@gmail.com>
2015-07-14 09:22:19 -0300

    dont run most tests on windows

--- runcmd_test.go
+++ runcmd_test.go
@@ -3,24 +3,38 @@ package main
 import (
 	"fmt"
 	"testing"
+    "runtime"
 )
+
 
 var FILES = []string{"a", "b", "space jam"}
 
 func TestRuncmdSimple(t *testing.T) {
-	if err := runCmdWithArgs("test", "cat $FILES", false, FILES); err != nil {
-        fmt.Println(err)
-		t.FailNow()
-	}
-    if err := runCmdWithArgs("test", "cat $FILES", true, FILES); err != nil {
-        fmt.Println(err)
+
+	if runtime.GOOS != "windows" {
+        if err := runCmdWithArgs("test", "cat $FILES", false, FILES); err != nil {
+            fmt.Println(err)
+    		t.FailNow()
+    	}
+    }
+    var cmd string
+    if runtime.GOOS == "windows" {
+        cmd = "type $FILES"
+    } else {
+        cmd = "cat $FILES"
+    }
+    if err := runCmdWithArgs("test", cmd, true, FILES); err != nil {
         t.FailNow()
     }
 }
 
 func TestRuncmdPiped(t *testing.T) {
-	if err := runCmdWithArgs("test", "cat $FILES | wc -l", true, FILES); err != nil {
-		fmt.Println(err)
-		t.FailNow()
-	}
+    var cmd string
+    cmd = "cat $FILES | wc -l"
+
+    if runtime.GOOS != "windows" {
+    	if err := runCmdWithArgs("test", cmd, true, FILES); err != nil {
+    		t.FailNow()
+    	}
+    }
 }
